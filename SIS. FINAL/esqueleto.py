fac23782eb7da9e278cdd014bb999c4b708b77e8
Keethlen <120063921+Keethlen@users.noreply.github.com>
2023-01-25 21:12:57 -0300

    projeto

---
+++ SIS. FINAL/esqueleto.py
@@ -0,0 +1,27 @@
+# cadastro pet shoop
+
+# tela inicial e cadastro 
+    #recebe: duas caixas.
+
+def principal():
+    pass
+   #print('Nome loja')
+
+def adicionar():
+    pass
+    #print('cadastro usuário')
+
+def listar():
+
+    #listar tipo_animal = [ 'Cachorro', 1, 'Gato', 2, 'hamster', 3, ]
+    #print(tipo_animal)
+
+def excluir():
+    pass
+
+def atualizar():
+    pass
+    #print('cadastro anterior')
+
+principal()
+
